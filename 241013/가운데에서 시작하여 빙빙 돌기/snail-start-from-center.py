818f1a28b1fee7fac4fa3369456ec7cc652cc388
seolhun <82288357+tjfgns6043@users.noreply.github.com>
2024-10-13 18:19:23 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 129ms, Memory: 25MB, Status: Passed - CodeTree

--- 241013/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
+++ 241013/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -4,23 +4,23 @@ arr = [[0 for _ in range(n)] for _ in range(n)]
 
 dir_num = 0
 
-dx = [0, -1, 0, 1] # 동 북 서 남
-dy = [1, 0, -1, 0]
+dx = [0, -1, 0, 1] # 서 북 동 남
+dy = [-1, 0, 1, 0]
 
-x, y = int(n/2), int(n/2)
-arr[x][y] = 1
+x, y = n-1, n-1
+arr[x][y] = n*n
 
-cnt = 1
+cnt = n*n
 
 def in_range(x,y):
     return x >= 0 and x < n and y >= 0 and y < n
 
-while cnt < n**2:
+while cnt > 1:
     nx = x + dx[dir_num]
     ny = y + dy[dir_num]
 
     if in_range(nx,ny) and arr[nx][ny] == 0:
-        cnt += 1
+        cnt -= 1
         arr[nx][ny] = cnt
         x, y = nx, ny
     else:
