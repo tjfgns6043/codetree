73731a4582cc3eb15964575a9dc254df66d48aa5
seolhun <82288357+tjfgns6043@users.noreply.github.com>
2024-10-13 22:02:51 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 156ms, Memory: 25MB, Status: Passed - CodeTree

--- 241013/중복되지 않는 숫자 중 최대/max-of-unique-number.py
+++ 241013/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -1,17 +1,22 @@
+# 변수 선언 및 입력:
+
 n = int(input())
+nums = list(map(int, input().split()))
 
-arr = list(map(int, input().split()))
+# 최댓값 찾기
+max_num = -1
 
-max_value = []
+for curr_num in nums:
+    # 최대가 될 수 있는 후보입니다.
+    if max_num < curr_num:
+        # 갱신할 수 있는지 확인하기 위해 이 숫자의 등장 빈도를 셉니다.
+        count = 0
+        for elem in nums:
+            if elem == curr_num:
+                count += 1
+        
+        # 이 숫자가 배열에서 유일할때만 갱신합니다.
+        if count == 1:
+            max_num = curr_num
 
-dp = [0 for _ in range(1001)]
-
-for i in range(len(arr)):
-    dp[arr[i]] += 1
-
-for i in range(1,1001):
-    if i == 1000 and dp[i] != 1:
-        print(-1)
-    if dp[i] == 1:
-        print(i)
-        break
+print(max_num)
