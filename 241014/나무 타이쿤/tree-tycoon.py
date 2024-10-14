9c2961a39f631c2d798b1e81b019f35bd7fc8d09
seolhun <82288357+tjfgns6043@users.noreply.github.com>
2024-10-14 23:23:36 +0900

    Title: 나무 타이쿤, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241014/나무 타이쿤/tree-tycoon.py
+++ 241014/나무 타이쿤/tree-tycoon.py
@@ -1,6 +1,6 @@
 n, m = map(int, input().split()) 
 
-arr = [list(map(int, input().split())) for _ in range(n)]
+arr =[list(map(int, input().split())) for _ in range(n)]
 
 special = [[0 for _ in range(n)] for _ in range(n)]
 
@@ -15,41 +15,46 @@ dys = [1, 1, 0, -1, -1, -1, 0, 1]
 growth = [(-1,1), (1,1), (1,-1), (-1,-1)] # 대각선
 
 def in_range(x,y):
-    return 0 <= x < n and 0 <= y < n
+    return x >= 0 and x < n and y >= 0 and y < n
 
 for i in range(m):
     # step1 특수 영양제를 이동 규칙에 따라 이동
     d, p = movings[i]
     d = d-1
-    new_special = [[0 for _ in range(n)] for _ in range(n)]
     for x in range(n):
         for y in range(n):
             if special[x][y] == 1:
-                nx = (x + dxs[d]*p) % n
-                ny = (y + dys[d]*p) % n
-                new_special[nx][ny] = 2
-    special = new_special
+                special[x][y] = 0
+                nx = (x + dxs[d] * p) % n
+                ny = (y + dys[d] * p) % n
+                special[nx][ny] = 2
                     
-    # step2 & 3 해당 땅에 특수 영양제를 투입하고 대각선 성장
+    # step2 해당 땅에 특수 영양제를 투입하고. 투입 후 투입된 특수 영양제는 사라집니다
+    # step3 투입한 리브로수의 대각선으로 인접한 방향에 높이가 1이상인 리브로수가 있는 만큼 높이가 더 성장
     for x in range(n):
         for y in range(n):
             if special[x][y] == 2:
                 special[x][y] = -1 # 영양제 투입 후 제거
                 arr[x][y] += 1
                 for dx, dy in growth: # 대각선
-                    nx, ny = x + dx, y + dy
-                    if in_range(nx,ny) and arr[nx][ny] >= 1:
-                        arr[x][y] += 1
-    
+                    nx = x + dx
+                    ny = y + dy
+                    if in_range(nx,ny):
+                        if arr[nx][ny] >= 1:
+                            arr[x][y] += 1
     # step4 특수 영양제 투입한 리브로수 제외 높이 2 이상인 리브로수는 높이 2를 베어서 잘라낸 리브로스로 특수 영양제를 사고 해당 위치에 특수 영양제를 올려둡니다
     for x in range(n):
         for y in range(n):
             if special[x][y] == -1:
                 special[x][y] = 0
-            elif arr[x][y] >= 2:
-                arr[x][y] -= 2
-                special[x][y] = 1
-
+                continue
+            else:
+                if arr[x][y] >= 2:
+                    arr[x][y] -= 2
+                    special[x][y] = 1
+total = 0
 # 최종 높이 출력
-total = sum(sum(row) for row in arr)
+for i in range(n):
+    for j in range(n):
+        total += arr[i][j]
 print(total)
